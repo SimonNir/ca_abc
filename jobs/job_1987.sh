e7e8632615f388ba987f935fd890ab5f5c0b72c0
Nirenberg S <nirenbergsd@065420d44cce.ornl.gov>
2025-07-24 14:38:34 -0400

    successful sweep

---
+++ jobs/job_1987.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=run_1987
+#SBATCH --output=logs/run_1987.out
+#SBATCH --error=logs/run_1987.err
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=01:00:00
+#SBATCH --mem=2G
+
+
+#SBATCH -p burst
+#SBATCH -A birthright
+
+echo "Running run_id=1987"
+source ~/abc_venv/bin/activate
+export PYTHONPATH=:/home/nirenbergsd/ca_abc
+python run_one.py 1987

5fc459d59cebe45e486b891b62f82c88607914d6
Nirenberg S <nirenbergsd@065420d44cce.ornl.gov>
2025-07-27 21:45:32 -0400

    true perturb

---
+++ examples/muller_brown/ca_perturb_sweep/jobs/job_80.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=abc_80
+#SBATCH --output=logs/abc_80.out
+#SBATCH --error=logs/abc_80.err
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=03:00:00
+#SBATCH --mem=3G
+#SBATCH -p burst
+#SBATCH -A birthright
+
+source ~/abc_venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+python run_one.py 69
